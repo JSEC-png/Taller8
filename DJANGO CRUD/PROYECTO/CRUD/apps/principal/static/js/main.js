34d8364ed26d6fd11ac66cbac323762ea9df0783
Sebastian <sebastian.rivera.leon2000@gmail.com>
2020-08-19 22:32:35 -0500

    Completo 100% + integrantes

--- DJANGO CRUD/PROYECTO/CRUD/apps/principal/static/js/main.js
+++ DJANGO CRUD/PROYECTO/CRUD/apps/principal/static/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('#pass2').blur(function () { 
         if ($('#pass2').val() != $('#pass').val()){
-            window.alert('Las constraseñas no coinciden, digite los dos ultimos campos nuevamente y haga click sobre cualquier otro elemento')
+            window.alert('Las constraseñas no coinciden, digite los dos ultimos campos nuevamente en orden y haga click sobre cualquier otro elemento')
             $('#btn').css('display','none');
         }
         else{
